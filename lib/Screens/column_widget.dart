d5ce15fb2a5d3d0e83f76e2695c02885354bd843
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-12-07 21:44:32 +0600

    column with new design add

--- lib/Screens/column_widget.dart
+++ lib/Screens/column_widget.dart
@@ -51,16 +51,64 @@ class Column_Widget extends StatelessWidget {
             ),
           ),
           Container(
-            margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
+              margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
               child: const Text(
-            "Nature is a British weekly scientific journal founded and based in London, England. As a multidisciplinary publication, Nature features peer-reviewed research from a variety of academic disciplines, mainly in science and technology.",
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 10,
-                fontWeight: FontWeight.normal,
-                letterSpacing: 2,
-                wordSpacing: 2),
-          ))
+                "Nature is a British weekly scientific journal founded and based in London, England. As a multidisciplinary publication, Nature features peer-reviewed research from a variety of academic disciplines, mainly in science and technology.",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 10,
+                    fontWeight: FontWeight.normal,
+                    letterSpacing: 2,
+                    wordSpacing: 2),
+                textAlign: TextAlign.justify,
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 30,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.phone),
+                      alignment: Alignment.center,
+                      padding: new EdgeInsets.all(0.0),
+                    ),
+                  ),
+                  Text(
+                    "Phone",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 30,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.share),
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints()
+                    ),
+                  ),
+                  Text(
+                    "Share",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
